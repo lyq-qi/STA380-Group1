953126e4153225c7f1132617d0639c356c4b7dcb
Shehzad-a <shehzad.h.ali@outlook.com>
2021-08-05 23:26:39 -0500

    Conclusion Report

--- greenBuildings.R
+++ greenBuildings.R
@@ -3,25 +3,20 @@ library(ggplot2)
 library(ggthemes)
 
 greenbuildings <- read.csv("greenbuildings.csv")
-greenbuildings$Green <- ifelse(greenbuildings$LEED == 1 | greenbuildings$Energystar ==1, "Green","Non-Green")
 #Create database that has only green buildings and one that has no green buildings
-greenOnly = greenbuildings %>% filter(green_rating == 1)
-nonGreen = greenbuildings %>% filter(LEED == 0 & Energystar == 0)
-
-#Create a variable to calculate the difference for green buildings vs average rent in their cluster
-greenOnly <- greenOnly %>%  mutate("greenRentVsClusterRent" = Rent-cluster_rent)
-medRentDiff <- median(greenOnly$greenRentVsClusterRent)
+greenOnly = greenbuildings %>% filter(green_rating == 1 & class_a == 0)
+nonGreen = greenbuildings %>% filter(green_rating == 0 & class_a == 0)
 
 #Calculate count, median rent, median leasing rate for green buildings
 greenCount <- greenOnly %>% summarise(n())
-medGreenCapacity <- greenbuildings %>%  filter((LEED == 1 | Energystar == 1)) %>% summarise(median(leasing_rate)/100)
-medGreenRent <- greenbuildings %>%  filter((LEED == 1 | Energystar == 1)) %>% summarise(median(Rent))
+medGreenCapacity <- greenOnly %>% summarise(median(leasing_rate)/100)
+medGreenRent <- greenOnly %>%   summarise(median(Rent))
 
 
 #Calculate count, median rent, median leasing rate for non-green buildings
-greenCount <- nonGreen %>% summarise(n())
-medNonGreenCapacity <- nonGreen %>%  filter((LEED == 0 & Energystar == 0)) %>% summarise(median(leasing_rate)/100)
-medNonGreenRent <- nonGreen %>%  filter((LEED == 0 & Energystar == 0)) %>% summarise(median(Rent))
+nonGreenCount <- nonGreen %>% summarise(n())
+medNonGreenCapacity <- nonGreen %>% summarise(median(leasing_rate)/100)
+medNonGreenRent <- nonGreen %>% summarise(median(Rent))
 
 #Calculate median gas and electric cost 
 medGasCost <- greenbuildings %>% summarise(median(Gas_Costs))
@@ -36,7 +31,7 @@ greenCostWeighted = .9
 #Calculate expected rent, expense, and profit if our building was green
 #Weight rent by expected capacity and calculate rent by adding rent diff b/w green/non green buildings
 #and adding that to the median non green rent. 
-greenExpectedRent <- (medNonGreenRent + medRentDiff) * medGreenCapacity * sqFt
+greenExpectedRent <- medGreenRent * medGreenCapacity * sqFt
 #Weight expenses by expected savings by using less resources
 greenExpectedExpense <- (sqFt * medGasCost * greenCostWeighted) + (sqFt * medElectricCost * greenCostWeighted)
 greenExpectedProfit <- greenExpectedRent - greenExpectedExpense
@@ -51,12 +46,28 @@ nonGreenExpectedProfit <- nonGreenExpectedRent - nonGreenExpectedExpense
 
 #Calculate the expected profit difference if the building chose to be green
 buildingDiff <- greenExpectedProfit-nonGreenExpectedProfit
-greenOnly$buildingDiff <- greenOnly$Rent - greenOnly$cluster_rent
+
+
 #Set green premium so we can calculate expected payback period and long-haul profits
 options("scipen" = 100,"digits" = 4) #Gets rid of scientific notation
 greenCert <- 100000000 *.05
 
 paybackPeriod = greenCert/buildingDiff
+
+print("Report")
+paste0("Expected rent price for Green building: ",medGreenRent, " Expected rent price for Non-Green building:",medNonGreenRent )
+paste0("Expected leasing rate for Green building: ",medGreenCapacity, " Expected rent price for Non-Green building:",medNonGreenCapacity )
+paste0("Expected yearly rent for Green building: ",round(greenExpectedRent,2), " Expected yearly rent for Non-Green building:", round(nonGreenExpectedRent,2))
+paste0("Expected electric Rate for the building: ",medElectricCost, " Expected gas Rate for the building: ",medGasCost )
+paste0("Expected electric/gas usage for the Green building: ",greenCostWeighted, " Expected electric/gas usage for the Non-Green Building: 1" )
+paste0("Expected expenses for the Green building: ",round(greenExpectedExpense,2), " Expected expenses for the Non-Green building: ",round(nonGreenExpectedExpense,2 ))
+paste0("Expected profit for Green Building: ",round(greenExpectedProfit,2), " Expected profit for Non-Green Building: ",round(nonGreenExpectedProfit,2) )
+paste0("Expected profit difference between a green and non-green building: ", round(buildingDiff,2))
+paste0("Expected payback period: ", round(paybackPeriod,2))
+
+
+
+
 
 layer1 <- ggplot(data = greenOnly, aes(x = cluster, y = buildingDiff)) 
 layer2 <- layer1 + geom_point() 
